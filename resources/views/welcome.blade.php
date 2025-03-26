dffea9f9c9dfa388eeff7803b69ab20b5c5f5791
unlunch <svamz34@gmail.com>
2025-03-26 22:47:56 +0700

    Initial commit: setup project structure

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel + Tailwind</title>
+    @vite(['resources/css/app.css'])
+</head>
+
+<body class="flex items-center justify-center min-h-screen bg-black">
+    <h1 class="text-4xl font-bold text-blue-600">Hello, Laravel with Tailwind CSS!</h1>
+</body>
+
+</html>
